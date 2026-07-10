99727af94611bd534779c9191719ed7a2fddc429
fubulatu-lang <fubulatu@gmail.com>
2026-07-10 18:03:08 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -34,22 +34,6 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
       }
     })
   ],
@@ -61,10 +45,5 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  },
-  build: {
-    target: 'es2020',
-    outDir: 'dist',
-    sourcemap: false
   }
 });
