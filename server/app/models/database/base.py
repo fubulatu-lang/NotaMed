c6eedbc0fd3043e8c4d4ef0c3102b3a765b80697
fubulatu-lang <fubulatu@gmail.com>
2026-07-14 14:17:24 +0000

    Update base.py

--- server/app/models/database/base.py
+++ server/app/models/database/base.py
@@ -8,6 +8,7 @@ from app.core.config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
+from app.models.database.note import Note  # noqa
 
 engine = create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
