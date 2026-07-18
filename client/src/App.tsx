cad83be496303776d81dba712dfe5739a40e7a84
fubulatu-lang <fubulatu@gmail.com>
2026-07-18 15:38:54 +0000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from './layouts/AppLayout';
 import DashboardPage from './features/dashboard/DashboardPage';
 import RecordingPage from './features/recording/RecordingPage';
+import SettingsPage from './features/settings/SettingsPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<AppLayout />}>
           <Route index element={<DashboardPage />} />
           <Route path="record" element={<RecordingPage />} />
+          <Route path="settings" element={<SettingsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
